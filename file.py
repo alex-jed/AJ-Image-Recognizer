03fc1b848efccc72490c4bd821caa7b7122dd5ee
alex-jed <alex.jednorowicz@gmail.com>
2024-08-02 20:41:09 +0100

    Get Image
    Added function to fetch required image and convert it to numpy matrix of RGB values

--- file.py
+++ file.py
@@ -1,1 +1,20 @@
+import numpy as np
+import os
+from PIL import Image
+
 print("READS/WRITES FILES")
+
+def get_image(image_filename):
+
+    """
+    Converts an image to a matrix of RGB values.
+    """
+
+    directory_name = os.path.dirname(os.path.abspath(__file__))
+    image_path = os.path.join(directory_name, f"{image_filename}.png")
+
+    with Image.open(image_path) as img:
+        img = img.convert('RGB')
+        rgb_matrix = np.array(img)
+
+    return rgb_matrix
