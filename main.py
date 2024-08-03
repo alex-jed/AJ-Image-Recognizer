5317c89dea766cb305a7cb54d945d3892e5dca3b
alex-jed <alex.jednorowicz@gmail.com>
2024-08-03 01:17:42 +0100

    added test script

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
-print("hello world!")
+import file
+import image_locator
+import image_recognizer
 
-print("9 + 10 =", 21)
+file.display_rbg_image(file.get_image("big image"))
+
+greyscale_matrix = file.rgb_to_grayscale(file.get_image("big image"))
+matricies = image_locator.find_text(greyscale_matrix)
+full_number_picture = []
+
+for count_i in range(len(matricies)):
+    row = []
+    #print("count_i", count_i)
+    for count_j in range(len(matricies[count_i])):
+        number_identified = image_recognizer.image_recogniser(matricies[count_i][count_j])
+        row.append(number_identified)
+    full_number_picture.append(row)
+
+
+for i in full_number_picture:
+    print(i)
